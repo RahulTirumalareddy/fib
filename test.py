2f5c561084e2389485f4a12b8d71dd1171708200
Rahul Reddy <Rahul Reddy>
2016-10-24 01:25:20 -0700

    First commit

---
+++ test.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+author='Rahul'
+def fibonacci(n):
+	a,b=0,1
+	if n==0:
+		return a
+	if n==1:
+		return b
+	while n>1:
+		a,b=b,a+b
+		n-=1
+	return b
+
+@app.route('/')
+def hello_world():
+	return render_template('index.html',author=author)
+
+@app.route('/answer', methods = ['POST'])
+def answer():
+	
+	case = int(request.form['n'])
+	return str(fibonacci(case))
+	#return redirect('/')
+
+if __name__ == '__main__':
+    app.run()
