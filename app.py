56cde397bd00fa75be85dd48ddb38ef84c477f69
RahulTirumalareddy <rahultimatum@gmail.com>
2016-10-25 10:31:29 -0700

    best version by far

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
 app = Flask(__name__)
-
+result=''
 def fibonacci(n):
 	a,b=0,1
 	if n==0:
@@ -13,20 +13,26 @@ def fibonacci(n):
 		n-=1
 	return b
 
-d={'fibonacci':fibonacci, 'square':'lambda x:x*x'}#different structure for lambda and def functions
+d={'fibonacci':fibonacci, 'square':'lambda x:x*x'}
 
 
 @app.route('/')
 def hello_world():
-	return render_template('index.html',d=d)
-#finish this
+	return render_template('index.html',d=d,result=result)
+
 @app.route('/add', methods=['POST'])
 def add():
-	d[request.form['func_name']]=request.form['func_def']
+	global result
+	if len(request.form['func_name'])>0:
+		d[request.form['func_name']]=request.form['func_def']
+		result='Function '+request.form['func_name']+' has been added.'
+	else:
+		result='The function name must be at least one character.'	
 	return hello_world()
 
 @app.route('/answer', methods = ['POST'])
 def answer():
+	global result
 	errors=[]
 	if not request.form['f'] in d:
 		errors.append("'" + request.form['f'] + "'"+ " is not in the dictionary of methods.")
@@ -34,16 +40,19 @@ def answer():
 	if not request.form['n'].isdigit():
 		errors.append("The input must be an integer.")
 	
-	if not errors and isinstance(d[request.form['f']],str):
+	if not errors:
 		f=d[request.form['f']]
-		case = request.form['n']
-		exp='('+f+')'+'('+ case+')'
-		return str(eval(exp))
-	elif not errors:
-		f=d[request.form['f']]
-		case = int(request.form['n'])
-		return str(f(case))
-	return " | ".join(errors)
+		n = request.form['n']
+		result=request.form['f'] + '(' + n + ')='
+		if isinstance(d[request.form['f']],str):
+			exp='('+f+')'+'('+n+')'
+			result+=str(eval(exp))
+			return hello_world()
+		else:
+			result+=str(f(int(n)))
+			return hello_world()
+	result=" | ".join(errors)
+	return hello_world()
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 33507))
