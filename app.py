ebb2301b67845abc4e8e4719c2347954aca3dd97
RahulTirumalareddy <rahultimatum@gmail.com>
2016-10-24 21:29:35 -0700

    support for cube

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
 app = Flask(__name__)
-author='Rahul'
-message='Enter n for fibonacci(n)'
+
 def fibonacci(n):
 	a,b=0,1
 	if n==0:
@@ -14,17 +13,36 @@ def fibonacci(n):
 		n-=1
 	return b
 
+d={'fibonacci':fibonacci, 'square':'lambda x:x*x'}#different structure for lambda and def functions
+
+
 @app.route('/')
 def hello_world():
-	return render_template('index.html',author=author,message=message)
+	return render_template('index.html',d=d)
+#finish this
+@app.route('/add', methods=['POST'])
+def add():
+	d[request.form['func_name']]=request.form['func_def']
+	return hello_world()
 
 @app.route('/answer', methods = ['POST'])
 def answer():
-	if request.form['n'].isdigit():
+	errors=[]
+	if not request.form['f'] in d:
+		errors.append("'" + request.form['f'] + "'"+ " is not in the dictionary of methods.")
+	
+	if not request.form['n'].isdigit():
+		errors.append("The input must be an integer.")
+	
+	if not errors and isinstance(d[request.form['f']],str):
+		f=d[request.form['f']]
+		case = request.form['n']
+		return str( eval( '('+f+')'+'('+ case+')' ) )
+	elif not errors:
+		f=d[request.form['f']]
 		case = int(request.form['n'])
-		return str(fibonacci(case))
-	else:
-		return render_template('index.html', author=author,message='Enter an integer')
+		return str(f(case))
+	return " | ".join(errors)
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 33507))
